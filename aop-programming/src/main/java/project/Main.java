2c6542d20a51bac0b21a18680eb29c8a8f32bba5
sangminLee <nalsm98@naver.com>
2020-03-03 13:46:53 +0900

    AOP 프로그래밍 세미나 프로젝트 업로드

--- aop-programming/src/main/java/project/Main.java
+++ aop-programming/src/main/java/project/Main.java
@@ -4,17 +4,41 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import project.service.CellPhonePay;
 import project.service.PaymentService;
 
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
     AnnotationConfigApplicationContext ctx =
         new AnnotationConfigApplicationContext(ProjectCtx.class);
 
-    PaymentService paymentService = ctx.getBean("paymentCellphoneService", PaymentService.class);
-    paymentService.execute();
+    Scanner scanner = new Scanner(System.in);
+    PaymentService paymentService = null;
 
-    paymentService = ctx.getBean("paymentSamsungPayService", PaymentService.class);
-    paymentService.execute();
+    while (true) {
+      System.out.println("========================");
+      System.out.println("결제 방식 선택");
+      System.out.println("1. 삼성페이 결제");
+      System.out.println("2. 휴대폰 결제");
+      System.out.println("3. 종료하기");
+      System.out.println("========================");
+      String menu = scanner.nextLine();
+      switch (menu) {
+        case "1":
+          paymentService = ctx.getBean("paymentCellphoneService", PaymentService.class);
+          paymentService.execute();
+          break;
+        case "2":
+          paymentService = ctx.getBean("paymentSamsungPayService", PaymentService.class);
+          paymentService.execute();
+          break;
+        case "3":
+          return;
+        default:
+          System.out.println("잘못된 입력입니다.");
+      }
+    }
+
   }
 
 }
